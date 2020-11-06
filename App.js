6e8cc5fe2598d0e85d015505ce9483df0c3befb5
taruncha <68689582+taruncha@users.noreply.github.com>
2020-11-06 13:14:52 +0530

    Update App.js

--- App.js
+++ App.js
@@ -7,9 +7,17 @@ function startTime() {
     amorpm = (hr < 12) ? "AM" : "PM";
     hr = (hr == 0) ? 12 : hr;
     hr = (hr > 12) ? hr - 12 : hr;
-   
+   if (hr<10) {
+       hr="0"+hr;
+   } 
+   if (min<10) {
+       min="0"+min;
+   }
+   if (sec<10) {
+       sec="0"+sec;
+   }
    
     document.getElementById("clock").innerHTML = hr + " : " + min + " : " + sec + " " + amorpm;
-    document.getElementById("date").innerHTML = today;
+  
     var time = setTimeout(function(){ startTime() }, 10);
 }
